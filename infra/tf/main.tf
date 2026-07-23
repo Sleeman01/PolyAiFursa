92eaa6231d878cdd970dde5615c90e6032102680
sleeman01 <eketehsleeman1@gmail.com>
2026-07-23 18:12:34 +0300

    feat: use S3 remote backend for terraform state

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket = "sleeman-polyai-k8s-tfstate"
+    key    = "k8s-cluster/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
